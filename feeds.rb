fba1ae576eb6b9bc95fdfc8a5595d8cfe6d62617
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2015-01-29 18:37:40 +0900

    add title and add emoji

--- feeds.rb
+++ feeds.rb
@@ -28,6 +28,8 @@ urls.each do |item|
 
   feed = Feedjira::Feed.fetch_and_parse(item['url'])
 
+  next if feed.entries.count < 1
+
   # using attributes
   feed.entries.each_with_index do |entry, i|
 
@@ -36,16 +38,24 @@ urls.each do |item|
 
     next if i == 0
 
-    #if updated >= 10.minute.ago
+    #if updated >= 2.year.ago
     if updated >= now.yesterday
 
-        rating_image = ''
-        for i in 1..entry.rating.to_i do
-          rating_image += "\u{2B50} "
-        end
+      if i == 1 && feed.entries.count > 1
+        bar_string = "\u{1F48C} \u{1F48C} \u{1F48C} \u{1F48C} \u{1F48C} "
+        system("curl -F channel=\##{$IRC_CHANNEL} -F message=\"#{bar_string} #{item['title']} #{bar_string} \" #{$IRC_URL}")
+      end
 
-        system("curl -F channel=\##{$IRC_CHANNEL} -F message=\"#{rating_image} [#{entry.version}] #{entry.author} #{entry.updated.getlocal}\" #{$IRC_URL}")
-        system("curl -F channel=\##{$IRC_CHANNEL} -F message=\"#{entry.title} - #{entry.content} #{}\" #{$IRC_URL}")
+      rating_image = ''
+      for i in 1..entry.rating.to_i do
+        rating_image += "\u{2B50} "
+      end
+
+      string1 = "curl -F channel=\##{$IRC_CHANNEL} -F message=\"#{rating_image} [#{entry.version}] #{entry.author} #{entry.updated.getlocal}\" #{$IRC_URL}"
+      string2 = "curl -F channel=\##{$IRC_CHANNEL} -F message=\"#{entry.title} - #{entry.content} #{}\" #{$IRC_URL}"
+
+      system(string1)
+      system(string2)
 
     end
 
